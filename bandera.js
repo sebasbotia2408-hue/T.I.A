fd5887238dfd9bd02483a68af54247e53488c983
sebasbotia2408-hue <sebasbotia2408@gmail.com>
2025-09-07 01:42:35 -0500

    Update bandera.js

--- bandera.js
+++ bandera.js
@@ -1,9 +1,13 @@
-document.querySelectorAll('#idiomas .opcion a').forEach(opcion => {
-  opcion.addEventListener('click', function(e) {
-    e.preventDefault();
-    const bandera = this.getAttribute('data-bandera');
-    document.getElementById('bandera-actual').src = bandera;
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelectorAll('#idiomas .opcion a').forEach(opcion => {
+    opcion.addEventListener('click', function(e) {
+      e.preventDefault();
+      
+      // obtener la ruta de la bandera
+      const bandera = this.getAttribute('data-bandera');
+      
+      // cambiar la bandera de arriba
+      document.getElementById('bandera-actual').src = bandera;
+    });
   });
 });
-
-
