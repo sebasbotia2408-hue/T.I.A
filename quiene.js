c4101b3aafc00ddfa97786edd8c60e92d5dce365
sebasbotia2408-hue <sebasbotia2408@gmail.com>
2025-09-15 21:25:25 -0500

    Update quiene.js

--- quiene.js
+++ quiene.js
@@ -3,6 +3,7 @@ const ListaIdioma = document.getElementById('idiomas');
 const idiomas = document.getElementsByClassName('opcion');
 
 
+
 idiomaActual.addEventListener('click',()=>{
     ListaIdioma.classList.toggle('toggle');
 });
@@ -23,48 +24,49 @@ function establecerIdioma(idioma) {
     const objetivosEspecificos = document.getElementById('ObjetivosEspecificos');
     const Mision = document.getElementById('Mision');
     const Vision = document.getElementById('Vision');
-    const inicio = document.getElementById('Inicio');
-    const contenido = document.getElementById('Contenido');
-    const imagenes = document.getElementById('Imagenes');
-    const videos = document.getElementById('Videos');
-    const denuncia = document.getElementById('Denuncia');
+    const PreguntaProblema = document.getElementById('PreguntaProblema')
+    const Inicio = document.getElementById('Inicio');
+    const Contenido = document.getElementById('Contenido');
+    const Imagenes = document.getElementById('Imagenes');
+    const Videos = document.getElementById('Videos');
+    const Denuncia = document.getElementById('Denuncia');
     switch (idioma) {
         case 'latino':
-            objetivoGeneral.textContent = 'OBJETIVO GENERAL';
-            objetivosEspecificos.textContent = 'OBJETIVOS ESPECIFICOS';
-            Mision.textContent = 'MISION';
-            Vision.textContent = 'VISION';
-            PreguntaProblema.textContent = 'PREGUNTA PROBLEMA';
-            inicio.textContent = 'Inicio';
-            contenido.textContent = 'Contenido';
-            imagenes.textContent = 'Imagenes';
-            videos.textContent = 'Videos';
-            denuncia.textContent = 'Denuncia';
+            objetivoGeneral.innerHTML = 'OBJETIVO GENERAL <img src="banderas/boton flecha.svg" alt="Icono boton">';
+            objetivosEspecificos.innerHTML = 'OBJETIVOS ESPECIFICOS <img src="banderas/boton flecha.svg" alt="Icono boton">';
+            Mision.innerHTML = 'MISION <img src="banderas/boton flecha.svg" alt="Icono boton">';
+            Vision.innerHTML = 'VISION <img src="banderas/boton flecha.svg" alt="Icono boton">';
+            PreguntaProblema.innerHTML = 'PREGUNTA PROBLEMA <img src="banderas/boton flecha.svg" alt="Icono boton">';
+            Inicio.textContent = 'Inicio';
+            Contenido.textContent = 'Contenido';
+            Imagenes.textContent = 'Imagenes';
+            Videos.textContent = 'Videos';
+            Denuncia.textContent = 'Denuncia';
 
             break;
 
         case 'usa':
-            objetivoGeneral.textContent = 'GENERAL OBJECTIVE';
-            objetivosEspecificos.textContent = 'SPECIFIC OBJECTIVES';
-            Mision.textContent = 'MISSION';
-            Vision.textContent = 'VISION';
-            PreguntaProblema.textContent = 'QUESTION PROBLEM';
-            inicio.textContent = 'Home';
-            contenido.textContent = 'Content';
-            imagenes.textContent = 'Images';
-            videos.textContent = 'Videos';
-            denuncia.textContent = 'Report';
+            objetivoGeneral.innerHTML = 'GENERAL OBJECTIVE <img src="banderas/boton flecha.svg" alt="Icono boton">';
+            objetivosEspecificos.innerHTML = 'SPECIFIC OBJECTIVES <img src="banderas/boton flecha.svg" alt="Icono boton">';
+            Mision.innerHTML = 'MISSION <img src="banderas/boton flecha.svg" alt="Icono boton">';
+            Vision.innerHTML = 'VISION <img src="banderas/boton flecha.svg" alt="Icono boton">';
+            PreguntaProblema.innerHTML = 'QUESTION PROBLEM <img src="banderas/boton flecha.svg" alt="Icono boton">';
+            Inicio.textContent = 'Home';
+            Contenido.textContent = 'Content';
+            Imagenes.textContent = 'Images';
+            Videos.textContent = 'Videos';
+            Denuncia.textContent = 'Report';
 
             break;
 
         default:
             titulo.textContent = 'TRÁFICO ILEGAL DE FAUNA SILVESTRE';
             parrafo.textContent = 'Este es un párrafo de información en español.';
-            inicio.textContent = 'Inicio';
-            contenido.textContent = 'Contenido';
-            imagenes.textContent = 'Imagenes';
-            videos.textContent = 'Videos';
-            denuncia.textContent = 'Denuncia';
+            Inicio.textContent = 'Inicio';
+            Contenido.textContent = 'Contenido';
+            Imagenes.textContent = 'Imagenes';
+            Videos.textContent = 'Videos';
+            Denuncia.textContent = 'Denuncia';
 
             break;
     }
@@ -84,3 +86,19 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
 });
+let imageVisible = null; 
+
+function mostrarImagen(num) {
+    const imgActual = document.getElementById("img" + num);
+
+    if (imageVisible === num) {
+        imgActual.style.display = "none";
+        imageVisible = null;
+        return;
+    }
+    if (imageVisible !== null) {
+        document.getElementById("img" + imageVisible).style.display = "none";
+    }
+    imgActual.style.display = "block";
+    imageVisible = num; 
+}
