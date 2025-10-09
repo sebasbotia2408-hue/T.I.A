2fec30ad36438b7e44a9ad58a10f912fefa473e6
sebasbotia2408-hue <sebasbotia2408@gmail.com>
2025-10-08 21:36:44 -0500

    Update denuncia.php

--- denuncia.php
+++ denuncia.php
@@ -1,26 +1,30 @@
 <?php
-$ubicacion = htmlspecialchars($_POST['ubicacion']);
-$especie = htmlspecialchars($_POST['especie']);
-$descripcion = htmlspecialchars($_POST['descripcion']);
-$anonimo = htmlspecialchars($_POST['anonimo']);
+$ubicacion = $_POST['ubicacion'];
+$especie = $_POST['especie'];
+$descripcion = $_POST['descripcion'];
+$anonimo = $_POST['anonimo'];
 
 $mensaje = "Ubicación del hecho: $ubicacion\n";
 $mensaje .= "Especie afectada: $especie\n";
-$mensaje .= "Descripción: $descripcion\n";
+$mensaje .= "Descripción del caso:\n$descripcion\n";
 $mensaje .= "¿Desea permanecer anónimo?: $anonimo\n";
 
-$destino = "juanaguillon02@gmail.com";
 
-$asunto = "Nueva denuncia de tráfico ilegal de fauna";
+$asunto = "Nueva denuncia de tráfico ilegal de animales";
 
-$headers = "From: formulario@tusitio.com\r\n";
-$headers .= "Reply-To: formulario@tusitio.com\r\n";
-$headers .= "X-Mailer: PHP/" . phpversion();
 
-if (mail($destino, $asunto, $mensaje, $headers)) {
-  echo "¡Denuncia enviada exitosamente!";
-} else {
-  echo "Hubo un error al enviar la denuncia.";
+$headers = "From: no-reply@denuncias.com\r\n";
+$headers .= "Reply-To: no-reply@denuncias.com\r\n";
+$headers .= "Content-type: text/plain; charset=UTF-8\r\n";
+
+
+$correos = explode(", ", "juanaguillon02@gmail.com, sabecerca@especie.com, denuncias@autoridadambiental.gov.co");
+
+
+foreach ($correos as $correo) {
+    mail(trim($correo), $asunto, $mensaje, $headers);
 }
 
+echo "<h2>Denuncia enviada correctamente. ¡Gracias por tu compromiso!</h2>";
 ?>
+
